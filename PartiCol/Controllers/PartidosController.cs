cff5b736e183a8f3edd053f9c025ee9ebbe22827
Salamank06 <santicosalamanca@gmail.com>
2025-08-15 15:32:23 -0500

    refactor: update PartidosController to include CRUD operations and modify Politico model

--- PartiCol/Controllers/PartidosController.cs
+++ PartiCol/Controllers/PartidosController.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PartiCol.Data;
+using PartiCol.Models;
 
 namespace PartiCol.Controllers;
 
@@ -39,7 +40,7 @@ public class PartidosController : ControllerBase
     // GET: /api/Partidos/5
     // Un partido por Id, incluyendo sus políticos
     [HttpGet("{id:int}")]
-    public async Task<IActionResult> GetPartido(int id)
+    public async Task<ActionResult<Partido>> GetPartido(int id)
     {
         var partido = await _context.Partidos
             .Include(p => p.Politicos)
@@ -47,5 +48,61 @@ public class PartidosController : ControllerBase
             .FirstOrDefaultAsync(p => p.Id == id);
 
         return partido is null ? NotFound() : Ok(partido);
+    }
+
+    // POST: api/Partidos
+    [HttpPost]
+    public async Task<ActionResult<Partido>> PostPartido(Partido partido)
+    {
+        _context.Partidos.Add(partido);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetPartido), new { id = partido.Id }, partido);
+    }
+
+    // PUT: api/Partidos/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutPartido(int id, Partido partido)
+    {
+        if (id != partido.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(partido).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!_context.Partidos.Any(e => e.Id == id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // DELETE: api/Partidos/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePartido(int id)
+    {
+        var partido = await _context.Partidos.FindAsync(id);
+        if (partido == null)
+        {
+            return NotFound();
+        }
+
+        _context.Partidos.Remove(partido);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
