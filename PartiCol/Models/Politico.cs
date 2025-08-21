b34082f990c8f3a1af1fcde85242d1f1fd6a5111
Salamank06 <santicosalamanca@gmail.com>
2025-08-21 02:37:15 -0500

    feat: implement CRUD operations for Politicos and enhance Partido model with validation

--- PartiCol/Models/Politico.cs
+++ PartiCol/Models/Politico.cs
@@ -1,11 +1,20 @@
-﻿namespace PartiCol.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace PartiCol.Models
 {
     public class Politico
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del político es obligatorio.")]
+        [StringLength(100)]
         public string Nombre { get; set; }
+
+        [StringLength(100)]
         public string CargoActual { get; set; }
         public string PerfilIdeologico { get; set; } = null!;
+
+        [StringLength(50)]
         public string? Twitter { get; set; }
 
         public int PartidoId { get; set; }
---
+++ PartiCol/Models/PoliticoDto.cs
@@ -0,0 +1,11 @@
+namespace PartiCol.Models;
+
+public class PoliticoDto
+{
+    public string? Nombre { get; set; }
+    public string? CargoActual { get; set; }
+    public string? PerfilIdeologico { get; set; }
+    public string? Twitter { get; set; }
+    public string? Partido { get; set; }
+    public string? IdeologiaPartido { get; set; }
+}
